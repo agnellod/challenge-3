c411b879e27f71ae6d3308385134c172a233911b
Dylan <agnello.dylan95@gmail.com>
2023-01-13 15:30:31 -0700

    adding all starter code

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "^!$%&|[](){}:;.,*+-#@<>~"
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
