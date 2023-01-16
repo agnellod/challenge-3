152a25b3b20000c575a9f1a99d7c3742fe0349bf
Dylan <agnello.dylan95@gmail.com>
2023-01-16 08:43:39 -0700

    adding final java

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # challenge-3
+## main changes
+- In this challenge I added java script in order to make the password generator usable.
+- First I added a few variables at the top to target for the characters for the password.
+- Then I added a function to generate the password.
+- After that I added a few more variables to create the prompt.
+- Then I added if statements so that if user didn't select any characters to restart prompt.
+-after that I created a few more variables to target different confirms on the prompt.
+- I added another if stating that the length can't be over a certain number or below another.
+-added another if statement with an alert stating you need to select at least one of the characters listed in the confirm statements.
+- I also added a for loop at the end of the function to generate the actual random password using variables I already created and put a return under so that the generated password would display at the very end.
+- Lastly I added one more if and else statement making sure that undefined wouldn't come back if you failed to select another character during the prompt.
--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-const characters = {
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "0123456789",
-  symbols: "^!$%&|[](){}:;.,*+-#@<>~"
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var symbols = "^!$%&|[](){}:;.,*+-#@<>~";
+  var generateBtn = document.querySelector("#generate");
+  
+
+
+function generatePassword() {
+  var generatedPassword = "";
+  var passwordLength = prompt ("How long do you want your password to be? Enter a number between 8 and 128");
+    if (passwordLength < 8 || passwordLength > 128 ){
+      alert ("Not enough characters please enter a number between 8 and 128");
+      
+    return;
+      }
+  var checkLower = confirm ("Do you want to include lowercase letters?");
+  var checkUpper = confirm ("Do you want to include uppercase letters?");
+  var checkNumbers = confirm ("Do you want to include numbers?");
+  var checkSymbols = confirm ("Do you want to include symbols?");
+
+    if (checkLower === false && checkUpper === false && checkNumbers === false && checkSymbols === false){
+      alert ("Need to select at least one other Character type")
+    
+    return;
+    }
+
+  for (let index = 0; index < passwordLength; index++) {
+    if (checkLower === true && generatedPassword.length < passwordLength){
+      var postion = Math.floor(Math.random() * lowercase.length);
+      generatedPassword = generatedPassword+lowercase[postion];
+    }
+    if (checkUpper === true && generatedPassword.length < passwordLength){
+      var postion = Math.floor(Math.random() * uppercase.length);
+      generatedPassword = generatedPassword + uppercase[postion];
+    }
+    if (checkNumbers === true && generatedPassword.length < passwordLength){
+      var postion = Math.floor(Math.random() * numbers.length);
+      generatedPassword = generatedPassword + numbers[postion];
+    }
+    if (checkSymbols === true && generatedPassword.length < passwordLength){
+      var postion = Math.floor(Math.random() * symbols.length);
+      generatedPassword = generatedPassword + symbols[postion];
+    }
+  }
+return generatedPassword;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    if(password !== undefined) {
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
